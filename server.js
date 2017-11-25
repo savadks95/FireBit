b5791f508733fd9ace7829d2288c99ba4032bfe4
savadks95 <savadks95@yahoo.com>
2017-11-25 18:33:32 +0530

    hihi

--- server.js
+++ server.js
@@ -12,30 +12,34 @@ var downloadLink;
 var fileName;
 var server;
 var parsed;
-var params;
+var param;
 var link;
 var port;
 port = process.env.PORT || 3000;
 
+app.get('/favicon.ico', function(req, res){
+console.log('favicon request recived');
+});
 app.get('/', function(req, res){
   fs.readFile('public/html/index.html', function (err, data) {
     res.write(data);
     res.end();
   });
 });
-app.get('/:thelink=', function(req, res){
+app.get('/:', function(req, res){
   fs.readFile('public/html/emptyRequest.html', function (err, data) {
     res.write(data);
     res.end();
   });
 });
-app.get('/:thelink=*', function(req, res){
+app.get('/*', function(req, res){
   res.send('what is');
+  console.log('app.getUrls',app.getUrls);
   //---------Reciving Url--------------------
-  params = url.parse(req.url, true).query;
-  console.log(params.thelink);
+  param = url.parse(req.url, true).query;
+  console.log(req.params);
   //downloadLink = params.thelink;
-  downloadLink=req.params.O;
+  downloadLink=req.params;
   
   //-----------------------------------------
   //------------checking for valid url-------
@@ -65,11 +69,14 @@ app.get('/:thelink=*', function(req, res){
     //
   }
 });
-app.get('*', function(req, res){
-  res.send('what', 404);
-});
+//------------------404--------------
+// app.get('*', function(req, res){
+//   res.send('what...error 404', 404);
+// });
+//------------------------------------
 app.listen(port);
-console.log('server up and running');
+
+console.log('server up and running', port);
 // if (app.get === '/favicon.ico', function(req, res){
 //    console.log('favicon');
 //  }) {
