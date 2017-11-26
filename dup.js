aeb718ecbe4385f671d8df9f7c57f12f534d6903
Saeeda <savadks95@yahoo.com>
2017-11-26 21:56:58 +0530

    dup

--- dup.js
+++ dup.js
@@ -61,61 +61,14 @@ app.get('*', function(req, res){
   }
   else {
     console.log('not url');
-    // fs.readFile('public/html/404.html', function (err, data) {
-    //   res.write(data);
-    //   res.end();
-    // });
-    //
+    fs.readFile('public/html/404.html', function (err, data) {
+      res.write(data);
+      res.end();
+    });
+    
   }
 }
 });
-// app.get('/l/:', function(req, res){
-//   fs.readFile('public/html/emptyRequest.html', function (err, data) {
-//     res.write(data);
-//     res.end();
-//   });
-// });
-// app.get('/l/:thelink*', function(req, res){
-//   res.send('what is');
-//   console.log('app.getUrls',app.getUrls);
-//   //---------Reciving Url--------------------
-//   // param = url.parse(req.url, true).query;
-//   console.log(req.params.thelink);
-//   downloadLink=req.params.thelink;
-  
-//   //-----------------------------------------
-//   //------------checking for valid url-------
-//   if (validUrl.isUri(downloadLink)) {
-//     console.log('Looks like an URI');
-
-//     //-----------------------------------------
-//     //----------Extracting filename-------------
-//     parsed = url.parse(downloadLink);
-//     fileName = path.basename(parsed.pathname);
-//     console.log(path.basename(parsed.pathname));
-//     //-------------------------------------------
-//     ///////////////Creating Torrent////////////////////
-//     webtorrentify(downloadLink)
-//       .then(function (buffer) {
-//          fs.writeFileSync(fileName + '.torrent', buffer);
-//          console.log('creating the torrent');
-//       });
-//     ////////////////////////////////////////////////
-//   }
-//   else {
-//     console.log('not url');
-//     // fs.readFile('public/html/404.html', function (err, data) {
-//     //   res.write(data);
-//     //   res.end();
-//     // });
-//     //
-//   }
-// });
-// //------------------404--------------
-// app.get('*', function(req, res){
-//   res.send('what...error 404', 404);
-// });
-// //------------------------------------
 
 app.listen(port);
 
